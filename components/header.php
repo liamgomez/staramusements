8e2fef7f4ceac3bc433c11c5f6a4c5c00ce19b11
peterlaxalt <pete@laxaltandmciver.co>
2016-09-21 23:47:51 -0700

    Build of all common styles and page except footer

--- components/header.php
+++ components/header.php
@@ -10,17 +10,17 @@
     <meta name="apple-mobile-web-app-capable" content="yes">
     <link rel="stylesheet" href="css/main.min.css">
     <script src="js/functions.js"></script>
-    <script src="js/jquery-3.0.0.min.js"></script>
-    <script src="js/typed.js"></script>
   </head>
   <body>
     <nav class="side-bar">
       <div class="bar-content">
         <div class="branding">
         </div>
-      </div>
-      <div class="insert-coin">
-        Insert Coin to Play
+        <div class="carat">
+        </div>
+        <div class="insert-coin">
+          Insert Coin to Play
+        </div>
       </div>
     </nav>
     <div class="wrapper">
--- components/page.php
+++ components/page.php
@@ -7,57 +7,71 @@
           </div>
         </div>
       </section>
-      <section class="row">
+      <section class="row teal-row">
         <div class="col-two-third">
-          <div class="content teal-block">
-            <h2>Discover</h2>
-            <h1>Variety</h1>
+          <div class="content block">
+            <div class="center-y">
+              <h2>Discover</h2>
+              <h3>Variety</h3>
+            </div>
           </div>
         </div>
         <div class="col-third">
-          <div class="content cream-block">
-            <h2>If you need</h2>
-            <h1 id="product-rotate"></h1>
-            <h2>We can get them</h2>
+          <div class="content cream-block  product-display-block">
+            <div class="center-y product-display">
+              <h2>If you need</h2>
+              <h1 id="product-rotate">Pool tables</h1>
+              <h2>We can get them</h2>
+            </div>
           </div>
         </div>
       </section>
       <section class="row">
-        <div class="col-full">
-          <div class="content navy-block focus-block">
-            <h2>No minimums</h2>
-            <h2>No maximums</h2>
-            <h2 class="red-head">More Amusement</h2>
+        <div class="col-full circle-row">
+          <div class="content navy-block block focus-block">
+            <div class="center-y">
+              <h2>No minimums</h2>
+              <h2>No maximums</h2>
+              <h2 class="red-head">More Amusement</h2>
+            </div>
           </div>
           <div class="circle-brand"></div>
         </div>
       </section>
       <section class="row">
         <div class="col-full">
-          <div class="content cream-block office-block">
-            <h2>Less stress, more productivity</h2>
-            <h1>Office options</h1>
-            <p>Work together, play together. Ask us about office options</p>
+          <div class="content cream-block block office-block">
+            <div class="center-y">
+              <h2>Less stress, more productivity</h2>
+              <h1>Office options</h1>
+              <p>Work together, play together. Ask us about office options.</p>
+            </div>
           </div>
         </div>
       </section>
       <section class="row">
         <div class="col-third">
-          <div class="content navy-block key-block">
-            <h1>Play</h1>
-            <p>From office teams to your customers, we all love fun</p>
+          <div class="content navy-block block key-block">
+            <div class="center-y fixed-y">
+              <h1>Play</h1>
+              <p>From office teams to your customers, we all love fun</p>
+            </div>
           </div>
         </div>
         <div class="col-third">
-          <div class="content gold-block key-block">
-            <h1>Work</h1>
-            <p>From office teams to your customers, we all love fun</p>
+          <div class="content gold-block block key-block">
+            <div class="center-y fixed-y">
+              <h1>Work</h1>
+              <p>Let us do the heavy lifting</p>
+            </div>
           </div>
         </div>
         <div class="col-third">
-          <div class="content teal-block key-block">
-            <h1>Trust</h1>
-            <p>From office teams to your customers, we all love fun</p>
+          <div class="content teal-block block key-block">
+            <div class="center-y fixed-y">
+              <h1>Trust</h1>
+              <p>Over 10 years of industry experience</p>
+            </div>
           </div>
         </div>
       </section>
